08a6744b69b469820ff9e1ebeefa6a7f7792871e
Akshay Sharma <akshaysharma@Akshays-MacBook-Pro.local>
2021-08-13 10:19:25 +0530

    change in server.js

--- utils.js
+++ utils.js
@@ -1,28 +1,36 @@
 const nightmare = require("nightmare");
 const cheerio = require("cheerio");
+const { prop } = require("cheerio/lib/api/attributes");
 const night = new nightmare({
   show: true,
+  gotoTimeout: 8000,
 });
 
-const contentPromise = (url) => {
-  console.log(url, "urlll");
-  return new Promise(function (resolve, reject) {
+const contentPromise = (urls) => {
+  let prom = [];
+  return new Promise(async function (resolve, reject) {
     let da;
-    night
-      .goto(url)
-      .wait(2000)
-      .evaluate(() => document.body.innerHTML)
-      .end()
-      .then(async (e) => {
-        console.log(e, "html body");
-        let $ = cheerio.load(e);
 
-        da = $(
-          "#__next > div > main > section.article.news.global-content > div > div.article-module.article > article.news.default-article.article > div.article-body-wrapper > section,.article-content > div,.article-body"
-        );
-      });
-    console.log(da, "daaaaa");
-    resolve(url);
+    urls.forEach(function (url) {
+      prom.push(
+        night
+          .goto(url)
+          .wait("body")
+          .evaluate(() => document.body.innerHTML)
+          .then(async (e) => {
+            let $ = cheerio.load(e);
+
+            da = $(
+              "#__next > div > main > section.article.news.global-content > div > div.article-module.article > article.news.default-article.article > div.article-body-wrapper > section,.article-content > div,.article-body"
+            ).text();
+            return da;
+          })
+          .catch((e) => console.log(e))
+      );
+    });
+
+    Promise.allSettled(prom).then((res) => console.log(res, "ressss"));
+    // console.log(da, "daaaaa");
     reject("Error");
   });
 };
